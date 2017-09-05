af1625305fbb1d936dbe440f58e66348a49a2bfd
rfstu <rfstu@users.noreply.github.com>
2017-09-05 18:05:18 +0200

    more license notes

--- app/src/main/java/org/secuso/privacyfriendlymath/database/PFASQLiteHelper.java
+++ app/src/main/java/org/secuso/privacyfriendlymath/database/PFASQLiteHelper.java
@@ -20,6 +20,21 @@ import java.util.List;
  * This class defines the structure of our database.
  */
 
+/**
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class PFASQLiteHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;

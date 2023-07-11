61a599b9b49f7cb19b8922e44b4b64a176c7da39
Patrick Schneider <urulu@student.kit.edu>
2023-07-11 11:06:07 +0200

    Updates to AndroidX

--- app/src/main/java/org/secuso/privacyfriendlyreckoningskills/activities/ScoreActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyreckoningskills/activities/ScoreActivity.java
@@ -15,12 +15,12 @@
 package org.secuso.privacyfriendlyreckoningskills.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.secuso.privacyfriendlyreckoningskills.R;
 
--- app/src/main/java/org/secuso/privacyfriendlyreckoningskills/activities/SplashActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyreckoningskills/activities/SplashActivity.java
@@ -16,7 +16,7 @@ package org.secuso.privacyfriendlyreckoningskills.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.secuso.privacyfriendlyreckoningskills.tutorial.TutorialActivity;
 

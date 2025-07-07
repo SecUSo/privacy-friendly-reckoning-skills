106a58349c298133a728700bf0b1133ef838ec98
Patrick Schneider <urulu@student.kit.edu>
2025-07-07 11:25:05 +0200

    Enable multidex.

--- app/src/main/java/org/secuso/privacyfriendlyreckoningskills/PFReckoningSkills.kt
+++ app/src/main/java/org/secuso/privacyfriendlyreckoningskills/PFReckoningSkills.kt
@@ -1,13 +1,13 @@
 package org.secuso.privacyfriendlyreckoningskills
 
-import android.app.Application
 import android.util.Log
+import androidx.multidex.MultiDexApplication
 import androidx.work.Configuration
 import org.secuso.privacyfriendlybackup.api.pfa.BackupManager
 import org.secuso.privacyfriendlyreckoningskills.backup.BackupCreator
 import org.secuso.privacyfriendlyreckoningskills.backup.BackupRestorer
 
-class PFReckoningSkills : Application(), Configuration.Provider {
+class PFReckoningSkills : MultiDexApplication(), Configuration.Provider {
 
     override fun onCreate() {
         super.onCreate()
